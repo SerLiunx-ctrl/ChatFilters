9dd10f6a3d83de07606c80bad2e60ed7dac6e943
SerLiunx-ctrl <17689543@qq.com>
2021-10-26 23:35:37 +0800

    新增语言文件支持.

--- src/main/java/me/serliunx/chatfilters/files/LanguageFile.java
+++ src/main/java/me/serliunx/chatfilters/files/LanguageFile.java
@@ -1,8 +1,19 @@
 package me.serliunx.chatfilters.files;
 
+import me.serliunx.chatfilters.ChatFilters;
+
 import org.bukkit.configuration.file.FileConfiguration;
 
+import java.io.File;
+
 public class LanguageFile implements IFiles{
+    private final ChatFilters plugin;
+    private FileConfiguration langConfig = null;
+    private File langConfigFile = null;
+
+    public LanguageFile(ChatFilters plugin){
+        this.plugin = plugin;
+    }
     @Override
     public void reloadConfig(boolean onReload) {
 
