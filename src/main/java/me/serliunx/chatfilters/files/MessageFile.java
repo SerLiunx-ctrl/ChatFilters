5c9df5c49444ef1bbe2660f95fdc5a99eb3f93c2
SerLiunx-ctrl <17689543@qq.com>
2021-10-26 13:06:14 +0800

    更新过滤器

--- src/main/java/me/serliunx/chatfilters/files/MessageFile.java
+++ src/main/java/me/serliunx/chatfilters/files/MessageFile.java
@@ -1,4 +1,20 @@
 package me.serliunx.chatfilters.files;
 
-public class MessageFile {
+import org.bukkit.configuration.file.FileConfiguration;
+
+public class MessageFile implements IFiles{
+    @Override
+    public void reloadConfig(boolean onReload) {
+
+    }
+
+    @Override
+    public void saveDefaultConfig() {
+
+    }
+
+    @Override
+    public FileConfiguration getConfiguration() {
+        return null;
+    }
 }
