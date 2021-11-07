7ef230ff582d9a77e34f4211e20d4c0874a100a2
SerLiunx <17689543@qq.com>
2021-11-07 20:41:31 +0800

    update sql,

--- src/main/java/me/serliunx/chatfilters/sql/Mysql.java
+++ src/main/java/me/serliunx/chatfilters/sql/Mysql.java
@@ -1,5 +1,17 @@
 package me.serliunx.chatfilters.sql;
 
+import me.serliunx.chatfilters.ChatFilters;
+
 public class Mysql extends SQL{
+    private static String userName;
+    private static String passWord;
+    private static String url;
+    private ChatFilters plugin;
+
+
+    public Mysql(ChatFilters plugin){
+        this.plugin = plugin;
+    }
+
 
 }
